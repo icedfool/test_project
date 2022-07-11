3064246d94eb01ed5255eeb51b7d7cc985d1dcc2
icedfool <icedfool@163.com>
2022-07-11 17:39:49 -0400

    create test_proj

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import i18n from './i18n'
+
+//Vue.config.productionTip = false
+
+/* new Vue({
+    i18n,
+    render: h => h(App)
+}).$mount('#app') */
+createApp(App).use(i18n).mount('#app')
